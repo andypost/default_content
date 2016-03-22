644b357b72ff28201b51f6176e33ae57e80d61db
Andy Postnikov <apostnikov@gmail.com>
2016-03-22 13:33:19 +0200

    Add service provider from EP

--- src/DefaultContentServiceProvider.php
+++ src/DefaultContentServiceProvider.php
@@ -9,6 +9,8 @@ namespace Drupal\default_content;
 
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
 use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * A default content service provider.
@@ -26,6 +28,21 @@ class DefaultContentServiceProvider extends ServiceProviderBase {
     $container
       ->getDefinition('rest.link_manager.relation')
       ->setClass('Drupal\default_content\LinkManager\RelationLinkManager');
+
+    $modules = $container->getParameter('container.modules');
+    // @todo Get rid of after https://www.drupal.org/node/2543726
+    if (isset($modules['taxonomy'])) {
+      // Add a normalizer service for term entities.
+      $service_definition = new Definition('Drupal\default_content\Normalizer\TermEntityNormalizer', [
+        new Reference('rest.link_manager'),
+        new Reference('entity.manager'),
+        new Reference('module_handler'),
+      ]);
+      // The priority must be higher than that of
+      // serializer.normalizer.entity.hal in hal.services.yml
+      $service_definition->addTag('normalizer', ['priority' => 30]);
+      $container->setDefinition('default_content.normalizer.taxonomy_term.halt', $service_definition);
+    }
   }
 
 }
